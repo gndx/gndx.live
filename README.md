8ca1a7e2f3d64f42a97bbb77d464ed29c1974f9b
Oscar Barajas <oscar@dragons.mx>
2021-02-15 14:43:34 -0500

    :art: [ADD] Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# gndx.live
+
+## Get started
+
+Install the dependencies...
+
+```bash
+cd gndx.live
+npm install
+```
+
+...then start [Rollup](https://rollupjs.org):
+
+```bash
+npm run dev
+```
---
+++ src/App.svelte
@@ -0,0 +1,52 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<div class="Header">
+		<div class="Header_container">
+			<div class="Header_container-logo">
+				<figure>
+					<img src="https://s3.amazonaws.com/gndx.dev/gndx.png" alt="Oscar Barajas @gndx">
+				</figure>
+			</div>
+			<div class="Header_container_nav">
+				<ul>
+					<li>Element</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="Calendar">
+		<div class="Calendar_container">
+			<h2>Calendar</h2>
+		</div>
+	</div>
+	<div class="Footer">
+		<div class="Footer_container">
+			
+		</div>
+	</div>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
